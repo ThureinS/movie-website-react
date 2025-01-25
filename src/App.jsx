7b7f2d31873c3c8838911d3571380d621c9b999a
ThureinS <94038134+ThureinS@users.noreply.github.com>
2025-01-26 03:00:15 +0630

    Fixed header typo

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
           <div className="wrapper">
               <header>
                   <img src="hero.png" alt="Hero Banner" />
-                  <h1>Find <span className="text-gradient">Moves</span> You&#39;ll Enjoy Without the Hassle</h1>
+                  <h1>Find <span className="text-gradient">Movies</span> You&#39;ll Enjoy Without the Hassle</h1>
               </header>
 
               <Search searchText={searchText} setSearchText={setSearchText} />
